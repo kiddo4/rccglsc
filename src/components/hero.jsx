6c4d0380504e78bbfaf4e12d069f8ac14224a44f
Olanrewaju taiwo <107349861+kiddo4@users.noreply.github.com>
2024-04-11 15:43:06 +0100

    updates

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,6 +5,9 @@ const Home = () => {
     <div>
       <section className="bg-white dark:bg-gray-900">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="lg:mt-0 mb-4 lg:mr-9 lg:col-span-5 lg:flex">
+            <img src="https://pbs.twimg.com/media/GKksGqRXIAAbxaU?format=jpg&name=large" alt="mockup" />
+          </div>
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Empowering Youth and Young Adults</h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Join us at RCCG Living Seed Church, where we are dedicated to raising kingdom generals and ambassadors among the youth and young adults. Experience a community anointed for dominance and transformation.</p>
@@ -13,9 +16,7 @@ const Home = () => {
               <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </a>
           </div>
-          <div className="lg:mt-0 mt-4 lg:mr-9 lg:col-span-5 lg:flex">
-            <img src="https://pbs.twimg.com/media/GKksGqRXIAAbxaU?format=jpg&name=large" alt="mockup" />
-          </div>
+          
         </div>
       </section>
 
@@ -49,7 +50,7 @@ const Home = () => {
         </div>
       </section>
 
-      {/* Add more sections as needed */}
+      
     </div>
   );
 };
